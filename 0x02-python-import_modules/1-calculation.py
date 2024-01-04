f120ef85152c6e7aee75e162687eeb36b49d3c83
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-01-04 16:27:25 +0300

    calc

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if  __name__ == "__main__":
+  from calculator_1.py import add, sub, mul, div
+  
+  a = 10
+  b = 5
+  addi = add(a, b)
+  subt = sub(a, b)
+  mult = mul(a, b)
+  divi = div(a, b)
+  print("{} + {} = {}".format(a, b, addi))
+  print("{} - {} = {}".format(a, b, subt))
+  print("{} * {} = {}".format(a, b, mult))
+  print("{} / {} = {}".format(a, b, divi))
