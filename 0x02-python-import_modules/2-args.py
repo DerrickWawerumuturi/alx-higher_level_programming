75523b6780a91a6bdfc02645163d11ad3d0ba577
DerrcikWawerumuturi <wawerumuturi57@gmail.com>
2024-01-04 09:14:40 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,38 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-<<<<<<< HEAD
-  import sys
-  lenargv = len(sys.argv) - 1
-  if lenargv == 1:
-    print("{} argument:".format(lenargv))
-  else:
-    if lenargv  == 0:
-      print("{} arguments:".format(lenargv))
-=======
     import sys
-    i = len(sys.argv) - 1
+    lenargv = (len(sys.argv)) - 1
 
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
->>>>>>> 18dea83cb974baee465a697b9294d1abd5c8442a
+    if lenargv == 1:
+        print("{} argument:".format(lenargv))
+    elif lenargv == 0:
+        print("{} arguments.".format(lenargv))
     else:
-        print("{} arguments:".format(i))
+        print("{} arguments:".format(lenargv))
 
-<<<<<<< HEAD
-
-    if lenargv >= 1:
-        lenargv = 0
-        for i in sys.argv:
-            if lenargv != 0:
-                print("{}: {}".format(lenargv, i))
-            lenargv += 1
-=======
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
->>>>>>> 18dea83cb974baee465a697b9294d1abd5c8442a
+    for i in range(1, lenargv + 1):
+        print("{}: {}".format(i, sys.argv[i]))
