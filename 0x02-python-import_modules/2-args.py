da989448d06d5a8991a74f07adaf304f5663b271
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-01-04 19:35:14 +0300

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-  import sys
-  lenargv = (len(sys.argv)) - 1
-  if lenargv == 1:
-    print("{} argument:".format(lenargv))
-  else:
-    if lenargv  == 0:
-      print("{} arguments:".format(lenargv))
-    else:
-      print("{} arguments:".format(lenargv))
+    import sys
+    i = len(sys.argv) - 1
 
-for i in range(1, lenargv + 1):
-  print("{}: {}".format(i, sys.argv[i]))
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
