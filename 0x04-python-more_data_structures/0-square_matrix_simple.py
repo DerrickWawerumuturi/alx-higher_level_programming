1511e49249456a6a5a3ca6d6d740e527d2822276
DerrcikWawerumuturi <wawerumuturi57@gmail.com>
2024-01-10 08:04:45 -0800

    append

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return (list(map(lambda row: list(map(lambda x: x**2, row)), matrix))) 
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+
+        for value in row:
+            new_row.append(value ** 2)
+
+        new_matrix.append(new_row)
+
+    return (new_matrix)
