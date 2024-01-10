fbfda15b52b6def9861e90518f0daba37d311958
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-01-10 21:32:39 +0300

    is

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,44 +1,13 @@
 #!/usr/bin/python3
-"""
-Return value of char
-"""
-
-
-def roman_value(char):
-    if char == 'I':
-        return 1
-    if char == 'V':
-        return 5
-    if char == 'X':
-        return 10
-    if char == 'L':
-        return 50
-    if char == 'C':
-        return 100
-    if char == 'D':
-        return 500
-    if char == 'M':
-        return 1000
-        return -1
-
-
-"""
-convert roman to int
-"""
-
-
 def roman_to_int(roman_string):
-    if (not roman_string) or \
-            type(roman_string) != str:
-        return None
-    number = 0
-    for i in range(len(roman_string)):
-        if i + 1 < len(roman_string):
-            if roman_value(roman_string[i]) \
-                    >= roman_value(roman_string[i + 1]):
-                number += roman_value(roman_string[i])
-            else:
-                number -= roman_value(roman_string[i])
+    if not roman_string or type(roman_string) != str:
+        return 0
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_n = 0
+    for j in range(len(roman_string)):
+        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
+            roman_n += roman_d[roman_string[j]] - 2 * \
+                        roman_d[roman_string[j - 1]]
         else:
-            number += roman_value(roman_string[i])
-    return number
+            roman_n += roman_d[roman_string[j]]
+    return roman_n
