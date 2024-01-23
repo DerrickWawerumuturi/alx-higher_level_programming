b34e04547d5b1edb7f03afd6ead609a733ad45e3
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-01-23 14:59:00 +0300

    sys

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
-
 def safe_print_integer_err(value):
     import sys
     try:
-        if type(value) == int:
-            print(value)
-            return True
-    except TypeError as e:
-        sys.stderr.write("Exception: {:d}".format(e))
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
