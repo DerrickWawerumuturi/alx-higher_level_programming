105bfca2fbb110202892031f5fc4f48480c63142
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-01-23 09:45:35 -0800

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" square class defination """
+class Square:
+    """ square class body """
+    def __init__(self, size):
+        """ Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
