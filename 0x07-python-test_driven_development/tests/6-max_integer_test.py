3201d528baf1722c7aa2c0fe399aa5700be440da
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-02-02 12:55:44 +0300

    matrix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,74 +1,54 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
-
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Test_maxinteger class to test the max_integer function"""
+    """Tests the function max_integer for correct output
+    """
 
-    def test_normals(self):
-        """Normal cases"""
-        self.assertEqual(max_integer([3, 4, 5]), 5)
-        self.assertEqual(max_integer([-6, -2, 5]), 5)
-        self.assertEqual(max_integer([-4, -32, -3]), -3)
-        self.assertEqual(max_integer([7, 7, 7]), 7)
+    def test_max_at_the_end(self):
+        """Tests all positive numbers
+        """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_list_is_empty(self):
-        """An empty list is given as parameter"""
+    def test_one_negative_number(self):
+        """Test when there is a negative number in the list
+        """
+        self.assertEqual(max_integer([1, 2, -1, 3, 4]), 4)
+
+    def test_only_negatives(self):
+        """Tests a list of only negative numbers in the list
+        """
+        self.assertEqual(max_integer([-4, -3, -2, -1]), -1)
+
+    def test_zero(self):
+        """Tests a list size of 0
+        """
         self.assertEqual(max_integer([]), None)
 
-    def test_max_int_list(self):
-        """"Find the max in a list of ints"""
-        list_1 = [1, 2, 3, 4]
-        self.assertEqual(max_integer(list_1), max(list_1))
-
-    def test_max_float_list(self):
-        """Find the max in a list of floats"""
-        list_2 = [2.3, 7.3, 2.6, 3.3, 5.9]
-        self.assertEqual(max_integer(list_2), max(list_2))
-
-    def test_max_float_int_list(self):
-        """Find the max in a list of int and floats"""
-        list_3 = [2.3, 7.3, 2.6, 3.3, 5.9, 4, 8, 11, 12]
-        self.assertEqual(max_integer(list_3), max(list_3))
-
-    def test_negative_list(self):
-        """Find the max in a list of negative int and floats"""
-        list_4 = [-2.3, -7.3, -2.6, -3.3, -5.9, -4, -8, -11, -12]
-        self.assertEqual(max_integer(list_4), max(list_4))
-
-    def test_argument_is_string(self):
-        """Raise TypeError if the argument is a string"""
-        with self.assertRaises(TypeError):
-            max_integer("Hello world", 1)
-
-    def test_argument_is_tuple(self):
-        """Raise TypeError if the argument is a tuple"""
-        with self.assertRaises(TypeError):
-            max_integer(1, 2, 3)
-
-    def test_argument_is_int_float(self):
-        """Raise TypeError if the argument is a int or float"""
-        with self.assertRaises(TypeError):
-            max_integer(3)
-        with self.assertRaises(TypeError):
-            max_integer(3.4)
-
-    def test_argument_is_boolean(self):
-        """Raise TypeError if the argument is a bool"""
-        with self.assertRaises(TypeError):
-            max_integer(True)
-
-    def test_special_chars(self):
+    def test_positive_and_negative(self):
+        """Test a list with positive and negative numbers
         """
-        Test error cases
+        self.assertEqual(max_integer([1, 6, 100, 4, 0, -1, 10]), 100)
+
+    def test_max_in_the_middle(self):
+        """Test when max is in the middle
         """
-        with self.assertRaises(TypeError):
-            max_integer("", "")
+        self.assertEqual(max_integer([2, 3, 6, 4, 5]), 6)
+
+    def test_max_at_the_beginning(self):
+        """Test with max at the beginning of list
+        """
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+
+    def test_list_with_one_element(self):
+        """Test with a list that only has 1 element
+        """
+        self.assertEqual(max_integer([1]), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
