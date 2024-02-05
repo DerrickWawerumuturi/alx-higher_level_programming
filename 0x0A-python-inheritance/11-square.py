ae4a4ad3bfc4fa43b327bf528456c6576332a8d6
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-02-05 18:17:56 +0300

    11 squares

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""
-defines a class that inherites """
+"""class Square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
 class Square(Rectangle):
-    """ Square class inherits from Rectangle """
+    """class Square"""
 
     def __init__(self, size):
-        """ Initializes a Square instance """
-        super().integer_validator("size", size)
+        """constructor method"""
+
+        self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
+        super().__init__(self.__size, self.__size)
 
     def area(self):
-        """ Computes the area of the square """
-        return self.__size * self.__size
+        """method to get area of Square"""
+
+        return super().area()
 
     def __str__(self):
-        """ Returns the square description """
+        """method to print readable str"""
+
         return "[Square] {}/{}".format(self.__size, self.__size)
