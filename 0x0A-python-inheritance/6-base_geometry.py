ad32d9ecc7dbad62cb2059413599267ab55a73c0
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-02-05 17:40:17 +0300

    public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Defines an class
+"""
+class BaseGeometry:
+    """
+    define a class body with a public instance method
+    """
+    def area(self):
+        """
+        raises:
+        exception : area() is not implemented
+        """
+        raise Exception("area() is not implemented")
