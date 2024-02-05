decd77cc3345c42a644a5139328f172d0a0878d5
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-02-05 18:21:15 +0300

    type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,14 +1,22 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""
+func to check if obj is exactly of given class
+"""
 
-"""Defines available attributes or method of an object ."""
 
 def is_same_class(obj, a_class):
-    """ returns True if object is an instance of a class
-    otherwise returns False 
-    Args:
-    obj : the object to check
-    a_class(type): The class to compare with
-    Returns:
-        bool:  True if obj is an instance of a_class. Otherwise, False.
+
     """
-    return isinstance(obj, a_class)
+    func to check if obj is exactly of given class
+
+    Args:
+        obj: object to be checked
+        a_class: class
+
+    Returns: True or False
+    """
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
