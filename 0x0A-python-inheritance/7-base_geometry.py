2c65aaa15e4f115d4981463b6df35657f4af1127
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-02-05 18:32:20 +0300

    base

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Defines an inherited class-checking method."""
+"""Defines class BaseGeometry."""
 
 
-def inherits_from(obj, a_class):
-    """Checks if an object is an inherited instance of a class.
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to compare.
-    Returns:
-        A boolean of inheritance.
-    """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+class BaseGeometry:
+    """Class body."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter format.
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
