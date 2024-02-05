fbc93411495bf08d6aca9bca702c8ccf737e08b4
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-02-05 18:31:55 +0300

    nherit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-""" method that checks if obj is an instance of a_class
-"""
+"""Defines an inherited class-checking method."""
 
 
 def inherits_from(obj, a_class):
-    """ method that checks if obj is an instance of a_class
-
+    """Checks if an object is an inherited instance of a class.
     Args:
-        obj: object
-        a_class: class 
-
-    Returns: True or False
+        obj (any): The object to check.
+        a_class (type): The class to compare.
+    Returns:
+        A boolean of inheritance.
     """
-    
-    if type(obj) is a_class:
-        return False
-    return isinstance(obj, a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
