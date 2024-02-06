1b59ce2c25717f6cd8b518a8ceb1382dc14f64a4
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-02-06 19:12:14 +0300

    append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" define a function
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    function takes in a string and appends it to teh end of the file
+    and returns the number of characters added
+    
+    Args:
+        filename: file to add string
+        text: string to be added to the file
+    """
+    with open(filename,  "a", encoding="UTF8") as file:
+        return file.write(text)
