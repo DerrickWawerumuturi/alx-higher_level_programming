68cba88b3bc62f28d8c09c990c23a04a16207dfb
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-02-09 15:15:16 +0300

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,6 +91,36 @@ class Rectangle(Base):
         for i in range(self.height):
             rectangle += (" " * self.x) + ("#" * self.width) + "\n"
         print(rectangle, end="")
+    def __str__(self):
+        """ 
+            returns a string 
+        """
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format
+                (self.id, self.__x, self.__y, self.__width, self.__height))
+    def update(self, *args, **kwargs):
+        if len(args) == 0:
+            dict = kwargs.keys()
+            if "height" in dict:
+                self.height = kwargs['height']
+            if "width" in dict:
+                self.width = kwargs['width']
+            if "x" in dict:
+                self.height = kwargs['x']
+            if "y" in dict:
+                self.y = kwargs['y']
+            if "id" in dict:
+                self.id = kwargs['id']
+        else:
+            if len(args) > 0:
+               self.id = args[0] 
+            if len(args) > 1:
+               self.width = args[1] 
+            if len(args) > 2:
+               self.height = args[2] 
+            if len(args) > 3:
+               self.x = args[x]
+            if len(args) >4:
+                self.y = args[4]
     @staticmethod
     def setter_validator(attribute, value):
         """
