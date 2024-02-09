f627491b73c8f6d106e3f058dc2c15d476f0f7a3
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-02-09 14:28:45 +0300

    rectangle basis

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""Rectangle class implement Base class """
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ rectangle class implement Base class
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """
+            Returning private attribute (__width)
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+            Setting private attribute (__width)
+        """
+        self.setter_validation("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+            Returning private attribute (___height)
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            Setting private attribute (__height)
+        """
+        self.setter_validation("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+            Returning private attribute (__x)
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+            Setting private attribute (__x)
+        """
+        self.setter_validation("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+            Returning private attribute (__y)
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+            Setting private attribute (__y)
+        """
+        self.setter_validation("y", value)
+        self.__y = value
+    @staticmethod
