a2f18a4f3b76193327877c247c9ef8467a607f16
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-02-12 11:17:43 +0300

    executable

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,76 +1,65 @@
 #!/usr/bin/python3
-
-""" 
-import the super class
+"""Define Square class implement Rectangle
 """
+
 from models.rectangle import Rectangle
 
-"""
-define the new class
-"""
+
 class Square(Rectangle):
-  def __init__(self, size, x=0, y=0, id=None):
-    """
-    call the super class from the Rectangle with id, x, y, size = width size= height
-    """
-    super().__init__(size, size, id, x, y)
+    """Square class body
+"""
 
-  def __str__(self):
-    return "[Square] ({:d}) {:d}/:d{} - {:d}".format(self.id, self.x, self.y, self.width)
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialization class props in constructor
+        """
+        super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """ return width size
+        """
+        return self.width
 
-  @property
-  def size(self)
-  """
-  define a getter for the size attribute
-  """
-    return self.width
-  @size.setter
-  def size(self, value):
-    """
-    define a setter for the size
-    """
-    if type(value) is not int:
-      raise  TypeError("width must be an integer")
-    if value <= 0:
-      raise ValueError("width must be > 0")
-    self.width = value
-    self.height = value
+    @size.setter
+    def size(self, value):
+        """module Square height and width
+        """
+        self.width = value
+        self.height = value
 
-  def  update(self, *args, **kwargs):
-    """
-    update method that assigns attributes 
-    """
-    if len(args) == 0:
-      dict =  kwargs.keys()
-      if 'size' in dict:
-        self.size = kwargs['size']
-      if 'x' in dict:
-        self.x = kwargs['x']
-      if 'y' in dict:
-        self.y = kwargs['y']
-      if 'id' in dict:
-        self.id = kwargs['id']
-      else:
-        length = len(args)
-        if length > 0:
-          self.id = args[0]
-        if length > 1:
-          self.size = args[1]
-        if length > 2:
-          self.x = args[2]
-        if length > 3:
-          self.y = args[3]
+    def __str__(self):
+        """Square class string
+        """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
 
+    def update(self, *args, **kwargs):
+        """update square props
+        """
+        if len(args):
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
 
     def to_dictionary(self):
-      """
-      returns a dictionary rep of a Square
-      """
-      return {
-        "id" : self.id,
-        "size": self.size,
-        "x": self.x,
-        "y" : self.y
-      }
+        """ return dict of class props
+        """
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
     
