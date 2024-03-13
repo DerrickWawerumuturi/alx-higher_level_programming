b4f85cf9cb74cb3ab4e9a4d4cf8c66a733b37830
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-03-13 20:15:17 +0300

    101

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,10 +1,15 @@
---script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows
-SELECT tv_shows.title
-FROM tv_shows
-WHERE tv_shows.id NOT IN (
-    SELECT tv_show_genres.show_id
-    FROM tv_genres
-    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-    WHERE tv_genres.name = 'Comedy'
-)
-ORDER BY tv_shows.title ASC;
+-- Prints records with column values in a set formed
+-- from the intersection of multiple tables
+SELECT DISTINCT title
+    FROM tv_shows
+    WHERE title NOT IN
+    (
+        SELECT tv_shows.title
+            FROM tv_shows
+            RIGHT JOIN tv_show_genres
+                ON tv_show_genres.show_id = tv_shows.id
+            INNER JOIN tv_genres
+                ON tv_genres.id = tv_show_genres.genre_id
+            WHERE tv_genres.name = 'Comedy'
+    )
+    ORDER BY title ASC;
