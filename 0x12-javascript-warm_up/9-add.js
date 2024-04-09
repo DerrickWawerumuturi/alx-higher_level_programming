58c15b685f69532c84a52664dcfa77b970fd334f
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-04-09 16:12:55 +0300

    cla

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,11 +3,12 @@
 const args = process.argv;
 
 function add (a, b) {
-  return (a + b);
+  return (parseInt(a) + parseInt(b));
 }
 
 if (args[2] && args[3]) {
-  console.log(add(args[2], args[3]));
+  let total = add(args[2], args[3]);
+  console.log(total);
 } else {
   console.log('NaN');
 }
