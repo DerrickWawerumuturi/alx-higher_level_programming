a9fb0e72b73107c702769d76d62851fa9fd07d35
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-04-09 11:41:38 +0300

    0

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 #!/usr/bin/node
 const myVar = 'Javascript is amazing';
 console.log(myVar);
-=======
-#!/usr/bin/node
-const myVar = "Javascript is amazing";
-console.log(myVar);
->>>>>>> cdd17791a1b7e87414cb00c5043f6a931ec03cad
+
