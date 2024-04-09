37b8ab1550594afa32fa76675bcb491ab6150f32
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-04-09 15:45:59 +0300

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (args[2] < 0) {
+  process.exit();
+} else if (typeof args[2] === 'number') {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < args[2]; i++) {
+    let row = '';
+    for (let j = 0; j < args[2]; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
