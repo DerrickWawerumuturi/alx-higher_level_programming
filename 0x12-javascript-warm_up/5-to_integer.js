0e003f126303369764d1854eb39118b3a1ae9a83
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-04-09 14:30:16 +0300

    not a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -8,7 +8,7 @@ function isIntStr (str) {
 
 if (args[2] === undefined) {
   console.log('Not a number');
-} else if (args[2] % 1 === 0 || args[2] !== 0) {
+} else if (Number.isInteger(args[2]) || !isNaN(args[2])) {
   console.log('My number:', Math.trunc(args[2]));
 } else if (isIntStr(args[2]) === true) {
   console.log('My number:', args[2]);
