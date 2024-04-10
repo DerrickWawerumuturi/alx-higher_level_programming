1af5897d85edf0b6063690190e87c5a32c5e036d
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-04-10 21:24:54 +0300

    X

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h >0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let row = 0;  row < this.height;  row++) {
+      let line = '';
+      for (let col = 0; col < this.width; col++) {
+        line += 'X';
+      }
+  }
+  console.log(line);
+}
+
+module.exports = Rectangle;
