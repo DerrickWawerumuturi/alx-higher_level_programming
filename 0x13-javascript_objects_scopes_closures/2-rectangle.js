2c4e1035a7f7d1f4ad93706fba9f616bbf37007b
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-04-10 20:55:53 +0300

    height

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,11 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w === 0 || h === 0 || w < 0 || h < 0) {
-      this.width = undefined;
-      this.heigth = undefined;
-    } else {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
   }
 }
+
+module.exports = Rectangle;
