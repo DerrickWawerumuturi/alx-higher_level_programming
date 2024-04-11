96c1664ca0fc844ff7d6df858d4bfa7b7a8d6e66
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-04-11 12:16:46 +0300

    base

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  if (base < 2 || base > 36) {
-    console.log('Invalid base');
-  }
-  return  function (num) {
-   return num.toString(base);
+  return function (num) {
+    return num.toString(base);
   };
 };
