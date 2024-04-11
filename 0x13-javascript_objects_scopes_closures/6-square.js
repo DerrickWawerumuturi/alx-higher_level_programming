f58db75b06be01576eed5ab5268a0e94b74aea58
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-04-11 11:00:38 +0300

    square1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const Square1 =  require('./5-square.js');
+
+class Square extends Square1 {
+  charPrint(c) {
+    if (c === undefined) {
+      c =  'X';
+    }
+    for (let row = 0; row < this.height; row++) {
+      let line = '';
+      for (let col = 0; col < this.width; col++) {
+        line += c;
+      }
+      console.log(line)	    
+    }
+  }
+}
+module.exports = Square;
