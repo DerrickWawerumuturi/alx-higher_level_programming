888f7dfbdd78c3cc9b114b1201d4dba2a156a8ee
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-04-13 06:05:24 +0300

    7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""script that lists all State objects from a database"""
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""script that prints the first State object from a database"""
 
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
     dbUser = sys.argv[1]
     dbPwd = sys.argv[2]
     dbName = sys.argv[3]
 
     engine = create_engine(f"mysql://{dbUser}:{dbPwd}@localhost:3306/{dbName}")
-    Session = sessionmaker(bind=engine)
-    # instance of Session
-    session = Session()
 
-    results = session.query(State).order_by(State.id.asc()).all()
-    for item in results:
-        print(f"{item.id}: {item.name}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State).order_by(State.id.asc()).first()
+
+    if res is not None:
+        print(f"{res.id}: {res.name}")
+    else:
+        print("Nothing")
