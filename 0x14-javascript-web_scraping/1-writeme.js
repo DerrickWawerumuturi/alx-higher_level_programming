41680bc2648d298c9fa14bc76b889479869653bc
Derrick Muturi <wawerumuturi57@gmail.com>
2024-05-22 11:25:32 +0300

    write file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+import { error } from "console";
+import {writeFile} from "fs/promises"
+import { argv } from "process";
+
+if (argv.length < 4) {
+    console.log('Usage: node read_file.js <file> <string>');
+    process.exit(1);
+}
+
+let file = argv[2];
+let data = argv[3];
+
+async function write_file() {
+    try {
+        writeFile(file, data, (err) => {
+            if (err) console.error(err);
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+write_file();
