386a2b04b6151dede40d3d2a574dd2e8caaf1700
Derrick Muturi <wawerumuturi57@gmail.com>
2024-05-22 14:11:08 +0300

    antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,29 +1,22 @@
 #!/usr/bin/node
 import request from "request";
-import { argv } from "process";
-import { error } from "console";
+import { argv, exit } from "process";
 
 let base_url = argv[2];
-const id = 18
-request(base_url, (error, response, body) => {
+if (argv.length !== 3) {
+    console.error('Usage: node app.js <film_id>');
+    exit(1)
+}
+const id = 18;
+request(base_url + "/", (error, response, body) => {
     if (error) {
-        console.error("Error: ", error);
+        console.error("Error is: ", error);
         return;
     }
 
-    const film = JSON.parse(body);
-    const charactersUrls = film.characters;
+    const films = JSON.parse(body).results;
+    const ch_Url = `https://swapi-api.alx-tools.com/api/people/${id}/`; 
 
-    const wedgeAntiles =  charactersUrls.find(`https://swapi-api.alx-tools.com/api/people/${id}`);
-
-    request(wedgeAntiles, (error, response, body) => {
-        if (error) {
-            console.error("Error: ", error);
-        }
-        const films = JSON.parse(body);
-        const number_films =  films.length;
-
-        console.log(number_films);
-    })
-
+    const films_with_wedge = films.filter(film => film.characters.includes(ch_Url));
+    console.log(films_with_wedge.length);
 })
