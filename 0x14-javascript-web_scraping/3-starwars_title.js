28a1428f298e74ad81bfb8fa988764a397f8a11d
Derrick Muturi <wawerumuturi57@gmail.com>
2024-05-22 12:37:14 +0300

    starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 import request from "request";
 import { argv } from "process";
-import { url } from "inspector";
-import { error } from "console";
 
-id = argv[2];
+let id = argv[2];
 let url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request(url, ( error, response, body) => {
@@ -12,5 +10,6 @@ request(url, ( error, response, body) => {
         console.error("Error: ", error);
         return;
     }
-    console.log(body);
-})
+    const film = JSON.parse(body);
+    console.log(film.title);
+;})
